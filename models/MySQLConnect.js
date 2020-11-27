b18079289499581686728a839e260527c52a6499
Stgal <maparilov@gmail.com>
2020-11-28 00:48:35 +0700

    использование конфига (для подключенимя к бд, номер порта на котором висит само приложение)

--- models/MySQLConnect.js
+++ models/MySQLConnect.js
@@ -1,20 +1,7 @@
 
 const Connetction = async (SQL_query) => {
     var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-
-        connectionLimit : 50,
-        connectTimeout  : 60000,
-        acquireTimeout  : 60000,
-
-        host     : '95.31.10.133',
-        port     : '3306',
-
-        user     : 'hackathon',
-        password : 'kks0Ta@elKKms',
-        database: 'hackathon',
-        dateStrings: 'TIMESTAMP',
-    });
+    var connection = mysql.createConnection(require('../config').mysql);
 
     connection.connect();
 
