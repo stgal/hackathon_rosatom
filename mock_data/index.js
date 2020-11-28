182e34093dfeecd775597767585fdebd088c9014
Stgal <maparilov@gmail.com>
2020-11-29 03:05:14 +0700

    mock_data order

--- mock_data/index.js
+++ mock_data/index.js
@@ -3,18 +3,31 @@ const moment = require('moment')
 const order = () => {
     var orders = []
 
-    let titleText = ['Разгрузить', 'Проверить', 'Обслужить', 'Починить']
-    let priority = ['MEDIUM', 'LOW', 'HIGH', 'CRITICAL']
+    let titleText = [
+        'Разгрузить',
+        'Проверить',
+        'Обслужить',
+        'Починить'
+    ]
+
+    let priority = [
+        'MEDIUM',
+        'LOW',
+        'HIGH',
+        'CRITICAL'
+    ]
 
     for (let i = 1; i < 80; i++) {
         orders.push({
             id: i,
+
             creatorId: Math.floor(Math.random() * Math.floor(10)),
+
             performer: 'All',
             createdData: moment().unix(),
             deadlineData: moment().unix() + 9999,
-            titleText: titleText[Math.floor(Math.random() * Math.floor(3))],
-            priority: priority[Math.floor(Math.random() * Math.floor(3))],
+            titleText: titleText[Math.floor(Math.random() * Math.floor(4))],
+            priority: priority[Math.floor(Math.random() * Math.floor(4))],
             orderType: 'TECHNOLOGICAL',
             orderStatus: 'New',
             bodyText: 'БЛА-БЛА-БЛА-БЛА-БЛА-БЛА-БЛА-БЛА ' + i
